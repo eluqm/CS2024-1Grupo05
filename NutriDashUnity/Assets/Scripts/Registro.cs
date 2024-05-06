2797b47c1711d37aa0c077d329d00cff7d3d7497
gvarlasalle <gampueror@ulasalle.edu.pe>
2024-05-05 21:11:25 -0500

    Add unity game
    Se adicionó todos los archivos del juego menos la carpeta Library (1.25 GB).

---
+++ NutriDashUnity/Assets/Scripts/Registro.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Networking;
+
+public class Registro : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField m_txtUusario = null;
+    [SerializeField] private TMP_InputField m_txtContrasena = null;
+
+    [SerializeField] private Button m_btnRegistrar = null; 
+    public void InvocarRegistro()
+    {
+        StartCoroutine(Registrar());
+    }
+
+    IEnumerator Registrar()
+    {
+        if (m_txtUusario.text != "" && m_txtContrasena.text != "")
+        {
+            WWWForm form = new WWWForm();
+            form.AddField("nombre", m_txtUusario.text);
+            form.AddField("contrasena", m_txtContrasena.text);
+
+            using UnityWebRequest w = UnityWebRequest.Post("http://localhost/nutridashphp/crearUsuario.php", form);
+            yield return w.SendWebRequest();
+
+            if (w.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError(w.error);
+            }
+            else
+            {
+                // Aquí es donde verificas la respuesta del servidor
+                string response = w.downloadHandler.text;
+                if (response.Contains("exito")) // Asumiendo que tu servidor devuelve "exito" en caso de éxito
+                {
+                    Debug.Log("La conexión a la base de datos fue exitosa.");
+                }
+                else
+                {
+                    Debug.Log("Hubo un error al conectar con la base de datos: " + response);
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("Los cuadros de texto no deben estar vacios.");
+        }
+    }
+
+
+}
