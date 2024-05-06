bef6b9f55464e90ac61d14c7e0e1e4c116efa4f4
gvarlasalle <112198852+gvarlasalle@users.noreply.github.com>
2024-05-05 20:56:37 -0500

    Add files php

---
+++ nutridashphp/crearUsuario.php
@@ -0,0 +1,30 @@
+<?php
+include "dbConeccion.php";
+
+$nombre = $_POST['nombre'];
+$contrasena = hash('sha256', $_POST['contrasena']);
+
+// Uso de consulta preparada para evitar inyecciones SQL
+$sql = "SELECT nombre FROM jugadores WHERE nombre = :nombre";
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['nombre' => $nombre]);
+
+if($stmt->rowCount() > 0)
+{
+    $data = array('done' => false , 'message' => "Error, nombre de usuario ya existe.");
+    Header('Content-Type: application/json');
+    echo json_encode($data);
+    exit();
+}
+else
+{
+    $sql = "INSERT INTO jugadores SET nombre = :nombre, contrasena = :contrasena";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(['nombre' => $nombre, 'contrasena' => $contrasena]);
+
+    $data = array('done' => true , 'message' => "Registro exitoso.");
+    Header('Content-Type: application/json');
+    echo json_encode($data);
+    exit();
+}
+?>
