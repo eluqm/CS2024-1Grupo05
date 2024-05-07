cdfedcc549b97db08981255fcd6af0ce2e0c3780
gvarlasalle <112198852+gvarlasalle@users.noreply.github.com>
2024-05-07 16:58:00 -0500

    Update dbConeccion.php

--- nutridashphp/dbConeccion.php
+++ nutridashphp/dbConeccion.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-    $pdo = new PDO('mysql:host=localhost;dbname=pruebadash', 'admin', '1234'); // Corregido aquí
+    $pdo = new PDO('mysql:host=localhost;dbname=pruebadash', 'admin', '1234');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET NAMES "utf8"');
 }catch(PDOException $e){
