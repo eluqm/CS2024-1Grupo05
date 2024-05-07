ef2c804d6e9197a36f8f72b1c8a2595378a14d5c
gvarlasalle <112198852+gvarlasalle@users.noreply.github.com>
2024-05-07 16:58:43 -0500

    Update dbConeccion.php

--- nutridashphp/dbConeccion.php
+++ nutridashphp/dbConeccion.php
@@ -1,6 +1,7 @@
 <?php
 
 try{
+    //Cadena de conexión, nombre de usuario y contraseña
     $pdo = new PDO('mysql:host=localhost;dbname=pruebadash', 'admin', '1234');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET NAMES "utf8"');
