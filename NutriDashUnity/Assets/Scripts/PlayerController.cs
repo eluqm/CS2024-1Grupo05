e4109433c9e9dcf8317e1f763443d0984640479e
José Rondón <joserondon0319@gmail.com>
2024-05-07 15:37:44 -0500

    Create PlayerController.cs

---
+++ NutriDashUnity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = 2.5f;
+
+    // References
+    private Rigidbody2D _rigidbody;
+    private Animator _animator;
+
+    // Movement
+    private Vector2 _movement;
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _movement = new Vector2(horizontalInput, 0f);        
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalVelocity = _movement.normalized.x * speed;
+        _rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y);
+    }
+
+    void LateUpdate()
+    {
+        _animator.SetBool("Idle", _movement == Vector2.zero);   
+    }
+}
