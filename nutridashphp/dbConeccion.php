e2f33e9278c8c6a767d5b0a1617e8ed2f7b621e9
gvarlasalle <112198852+gvarlasalle@users.noreply.github.com>
2024-05-07 16:59:37 -0500

    Update dbConeccion.php

--- nutridashphp/dbConeccion.php
+++ nutridashphp/dbConeccion.php
@@ -8,6 +8,7 @@ try{
     // Ejecución de una sentencia SQL directamente en la base de datos para configurar la codificación de caracteres
     $pdo->exec('SET NAMES "utf8"');
 }catch(PDOException $e){
+    // Captura de la excepción PDOException lanzada por un error en la base de datos
     echo "ERROR CONECTING TO DATABASE". $e->getMessage();
     exit();
 }   
