faeb4f14b56b5e4967ea455065b4224ce45d829d
José Rondón <joserondon0319@gmail.com>
2024-05-21 16:08:13 -0500

    Update PlayerController.cs

--- NutriDashUnity/Assets/Scripts/PlayerController.cs
+++ NutriDashUnity/Assets/Scripts/PlayerController.cs
@@ -2,65 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    // Velocidad del personaje.
     public float speed = 2.5f;
-    // Fuerza del salto del personaje.
     public float jumpForce = 2.5f;
 
-    // Transform del objeto que verifica si el personaje está en el suelo.
     public Transform groundCheck;
-    // Capa en la que se encuentra el suelo.
     public LayerMask groundLayer;
-    // Radio del círculo de verificación de suelo.
     public float groundCheckRadius;
 
     // References
-    // Referencia al Rigidbody2D del personaje.
     private Rigidbody2D _rigidbody;
-    // Referencia al Animator del personaje.
     private Animator _animator;
 
     // Movement
-    // Vector que almacena la dirección del movimiento.
     private Vector2 _movement;
-    // Booleano que indica si el personaje está mirando hacia la derecha.
     private bool _facingRight = true;
-    // Booleano que indica si el personaje está en el suelo.
     private bool _isGrounded;
 
-    // Método Awake, se llama cuando se inicializa el objeto.
+    // Points
+    private int points = 0;
+
     void Awake()
     {
-        // Obtiene el Rigidbody2D del objeto.
         _rigidbody = GetComponent<Rigidbody2D>();
-        // Obtiene el Animator del objeto.
         _animator = GetComponent<Animator>();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        //Movement
-        // Obtiene la entrada horizontal del teclado.
+        // Movement
         float horizontalInput = Input.GetAxisRaw("Horizontal");
-        // Crea un vector de movimiento con la entrada horizontal.
         _movement = new Vector2(horizontalInput, 0f);
-        
+
         // Flip Character
-        // Si la entrada horizontal es negativa y el personaje mira hacia la derecha.
-        if (horizontalInput < 0f && _facingRight == true) {
-            // Voltea el personaje.
+        if (horizontalInput < 0f && _facingRight)
+        {
             Flip();
-        // Si la entrada horizontal es positiva y el personaje no mira hacia la derecha.
-        } else if (horizontalInput > 0f && _facingRight == false) {
-            // Voltea el personaje.
+        }
+        else if (horizontalInput > 0f && !_facingRight)
+        {
             Flip();
         }
 
@@ -68,10 +49,10 @@ public class NewBehaviourScript : MonoBehaviour
         _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
         // Is Jumping?
-        if (Input.GetButtonDown("Jump") && _isGrounded == true) {
+        if (Input.GetButtonDown("Jump") && _isGrounded)
+        {
             _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-
     }
 
     void FixedUpdate()
@@ -93,5 +74,29 @@ public class NewBehaviourScript : MonoBehaviour
         float localScaleX = transform.localScale.x;
         localScaleX = localScaleX * -1f;
         transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    // Método para aplicar una disminución de velocidad y salto
+    public void ApplySpeedDecrease(float decreaseAmount, float jumpDecreaseAmount, float duration)
+    {
+        StartCoroutine(SpeedDecreaseCoroutine(decreaseAmount, jumpDecreaseAmount, duration));
+    }
+
+    private IEnumerator SpeedDecreaseCoroutine(float decreaseAmount, float jumpDecreaseAmount, float duration)
+    {
+        speed -= decreaseAmount;
+        jumpForce -= jumpDecreaseAmount;
+        Debug.Log($"Speed decreased to: {speed}, Jump force decreased to: {jumpForce}");  // Mensaje de depuración
+        yield return new WaitForSeconds(duration);
+        speed += decreaseAmount;
+        jumpForce += jumpDecreaseAmount;
+        Debug.Log($"Speed reset to: {speed}, Jump force reset to: {jumpForce}");  // Mensaje de depuración
+    }
+
+    // Método para agregar puntos
+    public void AddPoints(int pointsToAdd)
+    {
+        points += pointsToAdd;
+        Debug.Log("Puntos: " + points);
     }
 }
