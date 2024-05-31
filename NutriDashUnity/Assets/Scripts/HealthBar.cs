3a6b12a8381e94306bdbd712910a1420c7731451
José Rondón <joserondon0319@gmail.com>
2024-05-31 00:21:03 -0500

    Update HealthBar.cs

--- NutriDashUnity/Assets/Scripts/HealthBar.cs
+++ NutriDashUnity/Assets/Scripts/HealthBar.cs
@@ -5,4 +5,15 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    public Image healthBarFill;
+
+    public void SetMaxHealth(float health)
+    {
+        healthBarFill.fillAmount = 1;
+    }
+
+    public void SetHealth(float health)
+    {
+        healthBarFill.fillAmount = health / 100f;
+    }
 }
