341111bdd6dcb2a393c6cb8204203564f66c82b4
José Rondón <joserondon0319@gmail.com>
2024-06-21 03:35:37 -0500

    Update GameOverManager.cs

--- NUTRIDASH/Nutridash/Assets/Scripts/GameOverManager.cs
+++ NUTRIDASH/Nutridash/Assets/Scripts/GameOverManager.cs
@@ -6,25 +6,20 @@ public class GameOverManager : MonoBehaviour
     public GameObject gameOverPanel;
     public Player player; // Referencia al jugador
 
-    void Start()
+     void Start()
     {
         gameOverPanel.SetActive(false); // Asegurarse de que el panel de Game Over esté oculto al inicio
+        timer.StartTimer(); // Iniciar el temporizador al iniciar el GameOverManager
     }
 
     public void ShowGameOver()
     {
+        timer.StopTimer(); // Detener el temporizador al mostrar el Game Over
         gameOverPanel.SetActive(true);
         Time.timeScale = 0f; // Pausar el juego
     }
 
     public void Retry()
-    {
-        Time.timeScale = 1f; // Reanudar el tiempo del juego
-        player.ResetPlayer(); // Restablecer el estado del jugador
-        gameOverPanel.SetActive(false); // Ocultar el panel de Game Over
-    }
-
-    pu  public void Retry()
     {
         Time.timeScale = 1f; // Reanudar el tiempo del juego
         player.ResetPlayer(); // Restablecer el estado del jugador
