35f5e487ac2f6580538bd915a4baf758cc5f1180
José Rondón <joserondon0319@gmail.com>
2024-06-21 03:38:05 -0500

    Update Timer.cs

--- NUTRIDASH/Nutridash/Assets/Scripts/Timer.cs
+++ NUTRIDASH/Nutridash/Assets/Scripts/Timer.cs
@@ -3,4 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Timer : MonoBehaviour
 {
+public TextMeshProUGUI timerText; // Referencia al TextMeshPro donde se mostrará el temporizador
+
+    private float startTime; // Tiempo en que se inició el temporizador
+    private bool isTimerRunning; // Indica si el temporizador está corriendo
 }
